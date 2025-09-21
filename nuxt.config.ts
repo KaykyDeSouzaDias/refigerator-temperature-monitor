c756a987e3870e74315eb6322007e7d789eb699e
KaykyDeSouzaDias <kaykydias871@gmail.com>
2025-09-21 13:11:26 -0300

    Setup: instalação do tailwind e lucide icons no projeto

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'vuetify-nuxt-module']
-})
+  modules: ["vuetify-nuxt-module", "nuxt-lucide-icons"],
+  css: ["~/assets/css/main.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
