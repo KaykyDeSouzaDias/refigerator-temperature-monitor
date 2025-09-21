bf687a211d7324c92128538c1b8cd303ad93a3f8
KaykyDeSouzaDias <kaykydias871@gmail.com>
2025-09-21 13:13:58 -0300

    Feat: Login page implementation

---
+++ app/components/LoginForm.vue
@@ -0,0 +1,60 @@
+<script setup>
+const router = useRouter();
+const token = useCookie("token");
+
+const firstName = useState("first");
+const lastName = useState("last");
+
+const loading = useState("loading", () => false);
+
+function submit(event) {
+  loading.value = true;
+
+  token.value = "3ervejfko23k23m0fdi9-k09d3iw2mf0ij2f0j30odkok20odk02";
+  setTimeout(() => {
+    router.push("/");
+    loading.value = false;
+  }, 3000);
+}
+</script>
+
+<template>
+  <div class="h-30 flex flex-col items-center justify-center">
+    <h1 class="text-6xl font-semibold">Bem vindo</h1>
+    <h3>Por favor, entre com as suas credenciais abaixo.</h3>
+  </div>
+  <v-form @submit.prevent="submit">
+    <v-text-field
+      v-model="firstName"
+      label="Email"
+      variant="outlined"
+    ></v-text-field>
+
+    <v-text-field
+      v-model="lastName"
+      label="Senha"
+      variant="outlined"
+    ></v-text-field>
+
+    <div class="flex items-center justify-end">
+      <v-btn
+        variant="text"
+        size="small"
+        density="comfortable"
+        color="blue-darken-4"
+      >
+        <p class="font-semibold">Esqueceu a senha?</p>
+      </v-btn>
+    </div>
+
+    <v-btn
+      class="mt-4"
+      color="green-darken-2"
+      :loading="loading"
+      type="submit"
+      block
+    >
+      Entrar
+    </v-btn>
+  </v-form>
+</template>
---
+++ app/pages/auth/login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="w-full flex">
+    <v-img
+      aspect-ratio="16/9"
+      cover
+      src="https://cdn.pixabay.com/photo/2016/12/10/20/43/winter-1898134_1280.jpg"
+    ></v-img>
+    <div
+      class="h-lvh w-[40%] px-10 flex flex-col justify-center gap-4 bg-white"
+    >
+      <LoginForm />
+    </div>
+  </div>
+</template>
