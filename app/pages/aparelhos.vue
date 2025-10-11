de328eea8ef1d8fbf389250d1b06c5bd9739145c
KaykyDeSouzaDias <kaykydias871@gmail.com>
2025-10-11 17:35:46 -0300

    Fix: Device page layout visualization adjust

--- app/pages/aparelhos.vue
+++ app/pages/aparelhos.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { Cctv } from "lucide-vue-next";
+import { ChevronRight } from "lucide-vue-next";
 
 definePageMeta({
   layout: "dashboard",
@@ -7,18 +7,55 @@ definePageMeta({
 
 const { data, pending, error, refresh } = await useGetAllDevice();
 
-const books = useState("books", () => []);
+const books = useState("books", () => [
+  {
+    field2: "Aparelho 01",
+    field3: "SGA1-9KK94DK0",
+    field4: "São Gonçalo",
+    field5: 25.5,
+  },
+  {
+    field2: "Aparelho 02",
+    field3: "NIT2-4305MIK9",
+    field4: "Niterói",
+    field5: 43.5,
+  },
+]);
+const filters = ref({
+  search: "",
+  location: [],
+});
+
+const filteredBooks = computed(() => {
+  const searchFilter = filters.value.search;
+  const locationFilter = new Set(filters.value.location);
+
+  return books.value.filter(({ field2, field3, field4 }) => {
+    if (searchFilter) {
+      const check =
+        field2.includes(searchFilter) || field3.includes(searchFilter);
+      if (!check) return false;
+    }
+    if (locationFilter.size > 0) {
+      const check = locationFilter.has(field4);
+      if (!check) return false;
+    }
+
+    return true;
+  });
+});
 
 const headers = ref([
-  { title: "Título", key: "title", align: "start" },
-  { title: "Código", key: "code" },
-  { title: "Localização", key: "location" },
-  { title: "Actions", key: "actions", align: "end", sortable: false },
+  { title: "Nome", key: "field2", align: "start" },
+  { title: "Código", key: "field3" },
+  { title: "Localização", key: "field4" },
+  { title: "Temperatura (°C)", key: "field5" },
+  { title: "", key: "actions", align: "end", sortable: false },
 ]);
 
-watch(data, (newVal) => {
-  books.value = newVal;
-});
+// watch(data, (newVal) => {
+//   books.value = newVal;
+// });
 
 async function reset() {
   await fetch(
@@ -31,77 +68,76 @@ async function reset() {
 </script>
 
 <template>
-  <v-sheet border rounded>
-    <v-data-table
-      :headers="headers"
-      :hide-default-footer="books.length < 11"
-      :items="books"
-    >
-      <template v-slot:top>
-        <v-toolbar flat>
-          <v-toolbar-title>
-            <v-icon
-              color="medium-emphasis"
-              :icon="Cctv"
-              size="x-small"
-              start
-            ></v-icon>
+  <div class="flex flex-col gap-6">
+    <div class="h-[80px] flex items-center">
+      <h1 class="font-[Inter] font-semibold text-5xl">Aparelhos</h1>
+    </div>
 
-            Aparelhos
-          </v-toolbar-title>
-
-          <v-btn
-            prepend-icon="mdi-plus"
-            rounded="lg"
-            class="text-none"
-            text="Adicionar aparelho"
-            border
-            @click="add"
-          ></v-btn>
-        </v-toolbar>
-      </template>
-
-      <template v-slot:item.title="{ value }">
-        <v-chip
-          :text="value"
-          border="thin opacity-25"
-          prepend-icon="mdi-book"
-          label
-        >
-          <template v-slot:prepend>
-            <v-icon color="medium-emphasis"></v-icon>
-          </template>
-        </v-chip>
-      </template>
-
-      <template v-slot:item.actions="{ item }">
-        <div class="d-flex ga-2 justify-end">
-          <v-icon
-            color="medium-emphasis"
-            icon="mdi-pencil"
-            size="small"
-            @click="edit(item.id)"
-          ></v-icon>
-
-          <v-icon
-            color="medium-emphasis"
-            icon="mdi-delete"
-            size="small"
-            @click="remove(item.id)"
-          ></v-icon>
+    <v-sheet border rounded class="pa-4">
+      <div class="flex flex-col gap-4">
+        <div class="flex items-center gap-4">
+          <v-text-field
+            v-model="filters.search"
+            label="Pesquise pelo Nome ou Código..."
+            prepend-inner-icon="mdi-magnify"
+            variant="outlined"
+            density="compact"
+            hide-details
+            single-line
+          ></v-text-field>
+          <v-select
+            v-model="filters.location"
+            :items="books.map((book) => book.field4)"
+            label="Localização"
+            clearable
+            multiple
+            variant="outlined"
+            density="compact"
+            hide-details
+          />
         </div>
-      </template>
 
-      <template v-slot:no-data>
-        <v-btn
-          prepend-icon="mdi-backup-restore"
-          rounded="lg"
-          text="Reset data"
-          variant="text"
-          border
-          @click="reset"
-        ></v-btn>
-      </template>
-    </v-data-table>
-  </v-sheet>
+        <v-sheet border rounded>
+          <v-data-table
+            :headers="headers"
+            :items="filteredBooks"
+            :loading="pending"
+            :hide-default-footer="books.length < 11"
+          >
+            <template v-slot:item.title="{ value }">
+              <v-chip
+                :text="value"
+                border="thin opacity-25"
+                prepend-icon="mdi-book"
+                label
+              >
+                <template v-slot:prepend>
+                  <v-icon color="medium-emphasis"></v-icon>
+                </template>
+              </v-chip>
+            </template>
+
+            <template v-slot:item.actions="{ item }">
+              <v-icon-btn
+                variant="tonal"
+                rounded="lg"
+                width="40px"
+                height="30px"
+              >
+                <ChevronRight />
+              </v-icon-btn>
+            </template>
+
+            <template v-slot:no-data>
+              <v-empty-state
+                icon="mdi-magnify"
+                text="Tente filtrar novamente ou entre em contato com o seu administrador."
+                title="Nada foi encontrado"
+              ></v-empty-state>
+            </template>
+          </v-data-table>
+        </v-sheet>
+      </div>
+    </v-sheet>
+  </div>
 </template>
